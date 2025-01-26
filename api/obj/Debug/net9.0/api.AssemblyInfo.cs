94facb20c356c8539257627e45ab65a69b8940d7
CPR <christianpaulr.rosales@gmail.com>
2025-01-26 20:51:47 +0800

    chore: Update AssemblyInformationalVersion and cache inputs for build consistency

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+177fb0557e8058046315501950e914b183ac3b4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18e61702100da7a54c334088cd4c20aae54073b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
